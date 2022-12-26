7ce36cce564caac8cad23d6bf966871e9ec46905
kilian0001 <72407776+kilian0001@users.noreply.github.com>
2022-12-26 15:48:50 +0100

    Add files via upload

--- EasyBenchmark.cpp
+++ EasyBenchmark.cpp
@@ -1,53 +1,56 @@
+// made with <3 by kilian.#0001
+
 #include <iostream>
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
 
-int eingabe();
-int berechnung();
-int ausgabe();
+int input();
+int calculation();
+int output();
 
-double g = 0;
-double s = 1;
-double tmp = 0;
-int t = 0;
-double zeit = 0;
+double g = 0; // This is the maximum to count.
+double s = 1; // This is the 0.001 for Example.
+double tmp = 0; // A normal Temp
+int r = 0; // The remainde for the clear in line 45
+int modulo = 100; // Default 100. It clears the console while counting
+double zeit = 0; // For the timer
 
-int debug = 1; // Debug an = 1 / Debug aus = 0
+int debug = 1; // Debug on = 1 / Debug off = 0 I Let it on. Otherwise you have to take a realy high increment or maximum to see an effect.
 
 int main() {
     system("title EasyBenchmark.exe");
-    eingabe();
+    input();
     auto start = high_resolution_clock::now();
-    berechnung();
+    calculation();
     auto stop = high_resolution_clock::now();
-    ausgabe();
+    output();
     auto duration =duration_cast<microseconds>(stop - start);
     zeit = duration.count() / 1000000;
-    cout << "Das Programm hat " << zeit << "s gebraucht," << endl;
+    cout << "The Programm has taken " << zeit << "seconds." << endl;
     system("PAUSE");
     system("cls");
     main();
 }
 
-int eingabe() {
-    cout << "Geben Sie die Grenze und Schrittweite an \nGrenze: "; cin >> g; cout << "Schrittweite: "; cin >> s;
+int input() {
+    cout << "Give the maximum amount to count and the increment. \nMax Amount: "; cin >> g; cout << "Increment: "; cin >> s;
     return(0);
 }
 
-int berechnung() {
+int calculation() {
     for (int i = 0; tmp < g; i++) {
-        t = i % 100;
+        r = i % 100;
         tmp = tmp + s;
         if (debug == 1)
             cout << tmp << endl;
-        if (t == 0)
+        if (r == 0)
             system("cls");
     }
     return(0);
 }
 
-int ausgabe() {
-    cout << "Fertig" << endl;
+int output() {
+    cout << "Done!" << endl;
     return(0);
 }
